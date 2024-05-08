eb886b3fd6e8ae148d34e8f866c47603ea50598a
creynolds8 <cameron8.cr@gmail.com>
2024-05-07 21:23:31 -0700

    log profile inline due to how template literals are created

--- survey.js
+++ survey.js
@@ -8,25 +8,24 @@ const rl = readline.createInterface({
 const questions = [
   'What is your name? Or nickname if you have one?',
   'What is an activity you enjoy doing?',
-  'What music do you listen to while you do that?',
+  'What do you listen to while you do that?',
   'Which meal is your favorite?',
   'What do you like to eat for that meal?',
   'Which sport is your favorite?',
   'What is your superpower? Something you are great at.'
 ]
-const args = process.argv.slice(2)
-const profile = 'Devani loves listening to Ludovico Einaudi while coding, devouring Yak Momos for brunch, prefers Tennis over any other sport, and is amazing at dropping mad puns at inopportune times.'
+const answers = process.argv.slice(2)
 
 const askQuestion = function(index) {
   if (index === questions.length) {
-    console.log(args);
+    console.log(`${answers[0]} loves listening to ${answers[2]} while ${answers[1]}, devouring ${answers[4]} for ${answers[3]}, prefers ${answers[5]} over any other sport, and is amazing at ${answers[6]}.`);
     rl.close();
     return;
   }
   rl.question(questions[index], (answer) => {
     console.log(`${answer}, that's really cool.`);
     index++;
-    args.push(answer);
+    answers.push(answer);
     askQuestion(index);
   });
 };
